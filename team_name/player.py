ea56f8313d8c4970941709a83bee56b7e2a17f07
Sebastian <sdaveygargan@student.unimelb.edu.au>
2022-05-02 16:05:01 +1000

    added basic stuff

--- team_name/player.py
+++ team_name/player.py
@@ -1,7 +1,8 @@
-import numpy
+import referee.board
 
 class Player:
     def __init__(self, player, n):
+
         """
         Called once at the beginning of a game to initialise this player.
         Set up an internal representation of the game state.
@@ -10,25 +11,36 @@ class Player:
         play as Red, or the string "blue" if your player will play
         as Blue.
         """
-
+        self.player_colour = player
+        self.board = referee.board.Board(n)
 
     def action(self):
         """
         Called at the beginning of your turn. Based on the current state
         of the game, select an action to play.
         """
-        # put your code here
-    
+        # figure out change
+
+        #  don't need to apply it to your own board here
+
     def turn(self, player, action):
         """
-        Called at the end of each player's turn to inform this player of 
-        their chosen action. Update your internal representation of the 
-        game state based on this. The parameter action is the chosen 
-        action itself. 
-        
+        Called at the end of each player's turn to inform this player of
+        their chosen action. Update your internal representation of the
+        game state based on this. The parameter action is the chosen
+        action itself.
+
         Note: At the end of your player's turn, the action parameter is
         the same as what your player returned from the action method
         above. However, the referee has validated it at this point.
         """
-        # put your code here
+        if action[0] == "STEAL":
+            self.board.swap()
+        else:
+            self.board.place(player, (action[1], action[2]))
 
+    def alpha_beta_min(self):
+        pass
+
+    def alpha_beta_max(self):
+        pass
