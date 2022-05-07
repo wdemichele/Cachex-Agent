9fe2f2b547af2e8ea38de3a5954de1f18226b3c8
Sebastian <sdaveygargan@student.unimelb.edu.au>
2022-05-08 00:37:51 +1000

    renamed file

--- sebWIll/__init__.py
+++ sebWIll/__init__.py
@@ -5,4 +5,4 @@
 # example import below, you can define it in another file and import
 # it into this module with the name 'Player':
 
-from seb_will.player import Player
+from sebWIll.player import Player
